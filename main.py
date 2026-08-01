92dcbc5e1596c48c62245bc04b3dbce92f272c52
lahrour88 <abdelaadime@zohomail.com>
2026-08-01 16:42:31 +0200

    remove database and user classes; streamline main functionality

--- main.py
+++ main.py
@@ -1,36 +1,7 @@
-from user import user
-import database
-data =[{
-    'name' : 'John Doe',
-    'email' : 'johndoe@exemple.com ',
-    'phone' : '1234567890',
-    'password' : 'password123'
-},{
-    'name' : 'Jane Smith',
-    'email' : 'janesmith@exemple.com',
-    'phone' : '0987654321',
-    'password' : 'password456'
-},{
-    'name' : 'Alice Johnson',
-    'email' : 'alicejohnson@exemple.com',
-    'phone' : '1122334455',
-    'password' : 'password789'
-}]
-db=database.database()
-
-#create table
-print(db.create_table())
-print("-"*20)
-#add user 
-for users in data :
-    print(db.insert_user(user( name=users['name'], email=users['email'], phone=users['phone'], password=users['password'])))
-    print("-"*20)
-#update user
-print(db.update_user(query = "name='lahrour',password='updatedpassword'", id = 1))
-print("-"*20)
-#slect user an users
-print(db.select_user(colomn = "*", condition = "id =1"))
-print("-"*20)
-print(db.select_user(colomn = "*", condition = "id>0")) #or condition = ""
-print("-"*20)
-print(db.delete_user(id = 2))
+from database.database import Database
+from models.user import User
+from models.user import User
+user= User(table_name="users",name='miloud',email="miloud@mail.com",password ="miloud123@",phone="0615500436")
+db = Database("database.db")
+print(db.select_user("users", "*" ,condition="phone = '0615500436'"))
+db.close()
